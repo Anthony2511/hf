ddfa93d06e1746759bda281b7806a63b4b7a0ea3
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-02-04 14:17:18 +0100

    Added validation rules for field firstname

--- app/Http/Requests/AthleteRequest.php
+++ app/Http/Requests/AthleteRequest.php
@@ -26,7 +26,7 @@ class AthleteRequest extends FormRequest
     public function rules()
     {
         return [
-            // 'name' => 'required|min:5|max:255'
+            'firstname' => 'required|min:2|max:255'
         ];
     }
 
