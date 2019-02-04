a15843ac3eb586deeb50c025dd0019d903422778
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-02-04 13:29:45 +0100

    Migrate basic table athletes

---
+++ database/migrations/2019_02_04_110642_create_athletes_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateAthletesTable extends Migration {
+
+	public function up()
+	{
+		Schema::create('athletes', function(Blueprint $table) {
+			$table->increments('id');
+			$table->timestamps();
+			$table->softDeletes();
+			$table->string('firstname', 255);
+			$table->string('lastname', 255);
+			$table->date('date_of_birth');
+			$table->string('status', 255);
+			$table->string('slug', 255);
+		});
+	}
+
+	public function down()
+	{
+		Schema::drop('athletes');
+	}
+}
