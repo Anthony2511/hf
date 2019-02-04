5b7e1cd76a1ca0a13de97712d9d06a05bd961c7b
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-02-04 13:58:14 +0100

    Added slug

--- app/Models/Athlete.php
+++ app/Models/Athlete.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Backpack\CRUD\CrudTrait;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Cviebrock\EloquentSluggable\SluggableScopeHelpers;
 
 class Athlete extends Model
 {
     use CrudTrait;
+    use Sluggable, SluggableScopeHelpers;
 
     /*
     |--------------------------------------------------------------------------
@@ -28,6 +31,20 @@ class Athlete extends Model
     );
     // protected $hidden = [];
     // protected $dates = [];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'slug_or_title',
+            ],
+        ];
+    }
 
     /*
     |--------------------------------------------------------------------------
@@ -52,6 +69,18 @@ class Athlete extends Model
     | ACCESORS
     |--------------------------------------------------------------------------
     */
+
+    // The slug is created automatically from the "title" field if no slug exists.
+    public function getSlugOrTitleAttribute()
+    {
+        if ($this->slug != '') {
+            return $this->slug;
+        }
+        return $this->title;
+        $firstname = $this->firstname();
+        $lastname = $this->lastname();
+        return $firstname . '_' . $lastname;
+    }
 
     /*
     |--------------------------------------------------------------------------
