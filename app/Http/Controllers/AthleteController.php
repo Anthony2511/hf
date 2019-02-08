14be24aec1520a088104631fef3194095a568a81
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-02-08 12:24:06 +0100

    fct index to listing athletes

--- app/Http/Controllers/AthleteController.php
+++ app/Http/Controllers/AthleteController.php
@@ -7,5 +7,12 @@ use App\Models\Athlete;
 
 class AthleteController extends Controller
 {
-    //
+    public function index()
+    {
+        $athletes = Athlete::orderBy('lastname', 'ASC')->get();
+
+        return view('pages.athletes_index', [
+            'athletes' => $athletes
+        ]);
+    }
 }
