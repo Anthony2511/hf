2410664205a18de8ab54b979e359c8863a6594ec
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-02-22 15:46:40 +0100

    Added css partial trophies

--- resources/views/partials/single/athlete/trophies_athlete.blade.php
+++ resources/views/partials/single/athlete/trophies_athlete.blade.php
@@ -1,3 +1,14 @@
-<div class="athlete-trophies">
-    <p>Test</p>
-</div>
+<section class="athlete-trophies wrap">
+    <h4 class="title title__blue title__left title-size" aria-level="4" role="heading">Ses palmarès</h4>
+    <div class="athlete-trophies__container">
+        <div class="athlete-trophies__bloc">
+            <datetime class="athlete-trophies__date">2014</datetime> - <span>Champion de Belgique 1500m</span>
+        </div>
+        <div class="athlete-trophies__bloc">
+            <datetime class="athlete-trophies__date">2017</datetime> - <span>3ième au championnats d'Europe 1500m</span>
+        </div>
+        <div class="athlete-trophies__bloc">
+            <datetime class="athlete-trophies__date">2018</datetime> - <span>Champion LBFA 5000m</span>
+        </div>
+    </div>
+</section>
