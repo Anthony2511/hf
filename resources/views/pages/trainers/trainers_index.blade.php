05a88b473868feb0452d838393b407104d8acd60
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-02-26 11:43:58 +0100

    create partial for listing trainers also

--- resources/views/pages/trainers/trainers_index.blade.php
+++ resources/views/pages/trainers/trainers_index.blade.php
@@ -10,25 +10,6 @@
     @include('partials.intro-page')
     <div class="wrap">
         @include('partials.switchers.switcher-trainers')
-        <div class="home-athletes__container">
-            @foreach($trainers as $trainer)
-                <div class="home-athletes__bloc">
-                    <figure class="home-athletes__figure">
-                        <img src="{{ $trainer->getImageProfile('_profile.jpg') }}"
-                             alt="Image de l'athlète : {{$trainer->firstname}} {{$trainer->lastname}}"
-                             width="335"
-                             height="457">
-                    </figure>
-                    <section class="home-athletes__infos">
-                        <h4 class="home-athletes__infos-title">{{$trainer->firstname}}
-                            <span>{{$trainer->lastname}}</span></h4>
-                        <span class="home-athletes__disciplines">1500m, 5000m</span>
-                    </section>
-                    <a href="{{url('trainers/' . $trainer->slug )}}"
-                       class="home-athletes__link"
-                       title="Vers la page de l'athlète : {{$trainer->firstname}} {{$trainer->lastname}}"></a>
-                </div>
-            @endforeach
-        </div>
+        @include('partials.listing.listing-trainers')
     </div>
 @endsection
