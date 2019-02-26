1df402bfd7e5f2750145602190232f394d7ae6a2
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-02-26 11:32:18 +0100

    Added breadcrumb index trainers

--- resources/views/pages/trainers/trainers_show.blade.php
+++ resources/views/pages/trainers/trainers_show.blade.php
@@ -1,9 +1,17 @@
 @extends('layout')
 
+@section('header')
+    @include('partials.header')
+@endsection
 
-@section('class', 'index_athletes')
+@section('class', 'show_athletes')
 
 @section('content')
+    <div class="wrap">
+        <div class="breadcrumb">
+            <a href="{{ route('trainers') }}" class="breadcrumb__item">Retour aux entraîneurs</a>
+        </div>
+    </div>
     <div>
         Page d'un entraineur,ICI
     </div>
