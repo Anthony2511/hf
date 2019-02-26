3d813881ee57d55b25c7cfb18945c7aeb0443576
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-02-26 11:37:08 +0100

    Refactor listing athlete, create partial

--- resources/views/pages/athletes/athletes_index.blade.php
+++ resources/views/pages/athletes/athletes_index.blade.php
@@ -10,25 +10,6 @@
     @include('partials.intro-page')
     <div class="wrap">
         @include('partials.switchers.switcher-athletes')
-        <div class="home-athletes__container">
-            @foreach($athletes as $athlete)
-                <div class="home-athletes__bloc">
-                    <figure class="home-athletes__figure">
-                        <img src="{{ $athlete->getImageProfile('_profile.jpg') }}"
-                             alt="Image de l'athlète : {{$athlete->firstname}} {{$athlete->lastname}}"
-                             width="335"
-                             height="457">
-                    </figure>
-                    <section class="home-athletes__infos">
-                        <h4 class="home-athletes__infos-title">{{$athlete->firstname}}
-                            <span>{{$athlete->lastname}}</span></h4>
-                        <span class="home-athletes__disciplines">1500m, 5000m</span>
-                    </section>
-                    <a href="{{url('athletes/' . $athlete->slug )}}"
-                       class="home-athletes__link"
-                       title="Vers la page de l'athlète : {{$athlete->firstname}} {{$athlete->lastname}}"></a>
-                </div>
-            @endforeach
-        </div>
+        @include('partials.listing.listing-athletes')
     </div>
 @endsection
---
+++ resources/views/partials/listing/listing-athletes.blade.php
@@ -0,0 +1,20 @@
+<div class="home-athletes__container">
+    @foreach($athletes as $athlete)
+        <div class="home-athletes__bloc">
+            <figure class="home-athletes__figure">
+                <img src="{{ $athlete->getImageProfile('_profile.jpg') }}"
+                     alt="Image de l'athlète : {{$athlete->firstname}} {{$athlete->lastname}}"
+                     width="335"
+                     height="457">
+            </figure>
+            <section class="home-athletes__infos">
+                <h4 class="home-athletes__infos-title">{{$athlete->firstname}}
+                    <span>{{$athlete->lastname}}</span></h4>
+                <span class="home-athletes__disciplines">1500m, 5000m</span>
+            </section>
+            <a href="{{url('athletes/' . $athlete->slug )}}"
+               class="home-athletes__link"
+               title="Vers la page de l'athlète : {{$athlete->firstname}} {{$athlete->lastname}}"></a>
+        </div>
+    @endforeach
+</div>
