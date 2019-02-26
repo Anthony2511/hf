a63d5bae4de34850e03f95a11a9ccf244090c2cb
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-02-26 11:00:53 +0100

    Create view index_trainers

---
+++ resources/views/pages/trainers/trainers_index.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('header')
+    @include('partials.header')
+@endsection
+
+@section('class', 'index_athletes')
+
+@section('content')
+    @include('partials.intro-page')
+    <div class="wrap">
+        <p>Salut tout le monde</p>
+    </div>
+@endsection
