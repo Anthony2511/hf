3442999ea54bc4eabc761f9bb95b5b5745a37504
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-03-13 11:02:11 +0100

    Configure Division Model

--- app/Models/Division.php
+++ app/Models/Division.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Backpack\CRUD\CrudTrait;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Cviebrock\EloquentSluggable\SluggableScopeHelpers;
 
 class Division extends Model
 {
     use CrudTrait;
+    use Sluggable, SluggableScopeHelpers;
 
     /*
     |--------------------------------------------------------------------------
@@ -16,12 +19,35 @@ class Division extends Model
     */
 
     protected $table = 'divisions';
-    // protected $primaryKey = 'id';
-    // public $timestamps = false;
+    protected $primaryKey = 'id';
+    public $timestamps = true;
     // protected $guarded = ['id'];
-    protected $fillable = [];
+    protected $fillable = array(
+        'name',
+        'slug',
+        'gender'
+    );
     // protected $hidden = [];
     // protected $dates = [];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'slug_or_title',
+            ],
+        ];
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 
     /*
     |--------------------------------------------------------------------------
@@ -46,6 +72,18 @@ class Division extends Model
     | ACCESORS
     |--------------------------------------------------------------------------
     */
+    // The slug is created automatically from the "title" field if no slug exists.
+    public function getSlugOrTitleAttribute()
+    {
+        if ($this->slug != '') {
+            return $this->slug;
+        }
+
+        $name = $this->name;
+        $gender = $this->gender;
+
+        return $name . '-' . $gender;
+    }
 
     /*
     |--------------------------------------------------------------------------
