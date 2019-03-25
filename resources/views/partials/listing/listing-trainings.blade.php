bfaf02452d7b4621dd3c2d3b5a3d084d712e0675
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-03-25 12:29:51 +0100

    Added dynamic content & add place

--- resources/views/partials/listing/listing-trainings.blade.php
+++ resources/views/partials/listing/listing-trainings.blade.php
@@ -2,15 +2,18 @@
 <div class="trainings">
     <div class="trainigs__container">
         <div class="trainings__bloc">
+            @foreach($trainings as $training)
             <div class="trainings__single">
                 <div class="trainings__intro">
-                    <span class="trainings__day">Mardi</span>
-                    <span class="trainings__hour">17h30 - 19h00</span>
+                    <span class="trainings__day">{{ $training->day }}</span>
+                    <span class="trainings__hour">{{$training->startHour}} - {{$training->endHour}}</span>
                 </div>
-                <span class="trainings__type trainings__type--sprint">Sprint</span>
+                <div class="trainings__infos">
+                    <span class="trainings__place">{{$training->place->name}}</span>
+                    <span class="trainings__type trainings__type--sprint">{{$training->type->name}}</span>
+                </div>
                 <p class="trainings__text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing  elit. Duis nec odio velit.
-                    Morbi facilisis commodo leo, nec fermentum neque aliquam sit amet.
+                    {{$training->content}}
                 </p>
                 <a href="#" class="trainings__trainers">
                     <figure class="trainings__figure">
@@ -23,48 +26,7 @@
                     <span class="trainings__trainer-name">Jean Dupont</span>
                 </a>
             </div>
-            <div class="trainings__single">
-                <div class="trainings__intro">
-                    <span class="trainings__day">Mardi</span>
-                    <span class="trainings__hour">17h30 - 19h00</span>
-                </div>
-                <span class="trainings__type trainings__type--sprint">Sprint</span>
-                <p class="trainings__text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing  elit. Duis nec odio velit.
-                    Morbi facilisis commodo leo, nec fermentum neque aliquam sit amet.
-                </p>
-                <a class="trainings__trainers">
-                    <figure class="trainings__figure">
-                        <img src="../img/author.jpg"
-                             srcset="../img/author.jpg 2x"
-                             alt="Photo de l'entraineur"
-                             width="30"
-                             height="30">
-                    </figure>
-                    <span class="trainings__trainer-name">Jean Dupont</span>
-                </a>
-            </div>
-            <div class="trainings__single">
-                <div class="trainings__intro">
-                    <span class="trainings__day">Mardi</span>
-                    <span class="trainings__hour">17h30 - 19h00</span>
-                </div>
-                <span class="trainings__type trainings__type--sprint">Sprint</span>
-                <p class="trainings__text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing  elit. Duis nec odio velit.
-                    Morbi facilisis commodo leo, nec fermentum neque aliquam sit amet.
-                </p>
-                <a class="trainings__trainers">
-                    <figure class="trainings__figure">
-                        <img src="../img/author.jpg"
-                             srcset="../img/author.jpg 2x"
-                             alt="Photo de l'entraineur"
-                             width="30"
-                             height="30">
-                    </figure>
-                    <span class="trainings__trainer-name">Jean Dupont</span>
-                </a>
-            </div>
+            @endforeach
         </div>
     </div>
 </div>
