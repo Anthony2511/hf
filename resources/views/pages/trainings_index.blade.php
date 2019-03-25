b48b83b30c888ecbc422815a2e9a9b1f98902950
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-03-25 10:50:37 +0100

    Create view index_training

---
+++ resources/views/pages/trainings_index.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('header')
+    @include('partials.header')
+@endsection
+
+@section('class', 'index_training')
+
+@section('content')
+    @include('partials.intro-page')
+    <div class="wrap">
+        <p>SLLTTTTTT</p>
+    </div>
+@endsection
