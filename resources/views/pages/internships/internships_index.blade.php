a145b861603a8726d7cc5924546f7de50cfd544e
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-04-15 12:33:16 +0200

    Added partials switcher internship

--- resources/views/pages/internships/internships_index.blade.php
+++ resources/views/pages/internships/internships_index.blade.php
@@ -4,11 +4,12 @@
     @include('partials.header')
 @endsection
 
-@section('class', 'index_internships')
+@section('class', 'index_training')
 
 @section('content')
     @include('partials.intro-page')
     <div class="wrap">
+        @include('partials.switchers.switcher-internships')
         <p>SLT !!!!</p>
     </div>
 @endsection
