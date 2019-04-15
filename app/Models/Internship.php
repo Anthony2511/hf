c755be37663f35156c8ec605da3870ab3a845e7e
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-04-15 11:45:09 +0200

    Configure Model Internship

--- app/Models/Internship.php
+++ app/Models/Internship.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Backpack\CRUD\CrudTrait;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Cviebrock\EloquentSluggable\SluggableScopeHelpers;
 
 class Internship extends Model
 {
     use CrudTrait;
+    use Sluggable, SluggableScopeHelpers;
 
     /*
     |--------------------------------------------------------------------------
@@ -16,12 +19,39 @@ class Internship extends Model
     */
 
     protected $table = 'internships';
-    // protected $primaryKey = 'id';
-    // public $timestamps = false;
+    protected $primaryKey = 'id';
+    public $timestamps = true;
     // protected $guarded = ['id'];
-    protected $fillable = [];
+    protected $fillable = array(
+        'title',
+        'startDay',
+        'endDay',
+        'startHour',
+        'endHour',
+        'content'
+    );
     // protected $hidden = [];
     // protected $dates = [];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'slug_or_title',
+            ],
+        ];
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
 
     /*
     |--------------------------------------------------------------------------
@@ -46,6 +76,19 @@ class Internship extends Model
     | ACCESORS
     |--------------------------------------------------------------------------
     */
+
+    // The slug is created automatically from the "title" field if no slug exists.
+    public function getSlugOrTitleAttribute()
+    {
+        if ($this->slug != '') {
+            return $this->slug;
+        }
+
+        $title = $this->title;
+        $startDay = $this->startDay;
+
+        return $title . '-' . $startDay;
+    }
 
     /*
     |--------------------------------------------------------------------------
