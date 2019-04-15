4bb3ee4e998d1fd8948c91603403b517ca9c8976
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-04-15 12:16:25 +0200

    Create view index internship

---
+++ resources/views/pages/internships/internships_index.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('header')
+    @include('partials.header')
+@endsection
+
+@section('class', 'index_internships')
+
+@section('content')
+    @include('partials.intro-page')
+    <div class="wrap">
+        <p>SLT !!!!</p>
+    </div>
+@endsection
