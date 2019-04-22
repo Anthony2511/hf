1912532d155128f7ba054717c1558a48534375db
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-04-22 14:04:00 +0200

    Float label in css form internship

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,6 +9,7 @@
 
 @include('partials.footer')
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="{{ asset('js/jquery-accessibleMegaMenu.js') }}"></script>
 <script src="{{ asset('js/configAccessibleMegaMenu.js') }}"></script>
 </body>
--- resources/views/partials/single/internship/form_internship.blade.php
+++ resources/views/partials/single/internship/form_internship.blade.php
@@ -1,6 +1,20 @@
-<section class="single-stage__bloc-form">
+<section class="single-stage__bloc-form wrap">
     <h2 aria-level="2" role="heading" class="title title__blue title__center">Formulaire de préinscription</h2>
     {{ Form::open([ 'method' => 'POST', 'class' => 'single-stage__form']) }}
-
+    <div class="single-stage__form-container">
+        <legend class="single-stage__form-legend">Tous les champs suivis d'un (*) sont obligatoires</legend>
+        <div class="single-stage__form-bloc">
+            <div class="floating-label">
+                <input class="single-stage__form-input" placeholder="Nom complet du parent *"
+                       type="text" name="name" id="name" autocomplete="off">
+                <label for="name">Nom complet du parent *</label>
+            </div>
+            <div class="floating-label">
+                <input class="single-stage__form-input" placeholder="Votre email *"
+                       type="text" name="email" id="email" autocomplete="off">
+                <label for="email">Votre email *</label>
+            </div>
+        </div>
+    </div>
     {{ Form::close() }}
 </section>
