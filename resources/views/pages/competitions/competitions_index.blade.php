1f2268322196281c02745e8827a6a08845814521
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-04-25 15:10:30 +0200

    Create view index competitions

---
+++ resources/views/pages/competitions/competitions_index.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('header')
+    @include('partials.header')
+@endsection
+
+@section('class', 'index_competitions')
+
+@section('content')
+    <div class="wrap">
+        <p>SLT !!!!</p>
+    </div>
+@endsection
