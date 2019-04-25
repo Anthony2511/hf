6b6dc9b15b8b03fb7b4d8dbcf5d74272536e0115
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-04-25 15:30:38 +0200

    Added partials intro competitions

--- resources/views/pages/competitions/competitions_index.blade.php
+++ resources/views/pages/competitions/competitions_index.blade.php
@@ -4,9 +4,10 @@
     @include('partials.header')
 @endsection
 
-@section('class', 'index_competitions')
+@section('class', 'index_athletes')
 
 @section('content')
+    @include('.partials.intros.intro-competition')
     <div class="wrap">
         @include('partials.switchers.switcher-competitions')
         @include('partials.listing.listing-competitions')
---
+++ resources/views/partials/intros/intro-competition.blade.php
@@ -0,0 +1,24 @@
+<div class="intro">
+    <figure class="intro__bloc-img" style="background-image: url('http://hf.test/img/image-training.jpg')">
+        <img src="../img/image-training.jpg"
+             srcset="../img/image-training.jpg 2x"
+             alt="Image d'introduction pour les compétitions"
+             width="614"
+             height="409"
+             class="hidden">
+    </figure>
+    <div class="intro__bloc wrap">
+        <div class="intro__bloc-text">
+            <h2 class="title title__blue title__left" aria-level="2" role="heading">Les compétitions</h2>
+            <p class="intro__text">
+                Vous retrouvez ici le planning de toutes les compétitions prévues et terminées. Venez aussi jeter un oeil
+                aux entraînements afin de voir si vous avez assez de temps pour vous entraîner.
+            </p>
+            <a href="{{route('entrainements')}}" class="button" title="Vers les entraînements">
+                <span class="button-blue__left">Voir nos entraînements</span>
+                <i class="button-blue__right"></i>
+            </a>
+        </div>
+    </div>
+
+</div>
