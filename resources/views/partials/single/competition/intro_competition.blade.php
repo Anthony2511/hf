1a37a8ece49fc002b1f1cae3ead8ea5e5ade6d00
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-04-29 11:14:14 +0200

    Added css for partial intro competition

--- resources/views/partials/single/competition/intro_competition.blade.php
+++ resources/views/partials/single/competition/intro_competition.blade.php
@@ -1,4 +1,4 @@
-<div class="intro-competition wrap">
+<div class="intro-competition">
     <h2 aria-level="2" role="heading" class="title title__blue title__center">{{ $competition->title }}</h2>
     <div class="intro-competition__container">
         <figure class="intro-competition__bloc-img" style="background-image: url('http://hf.test/img/image-compet.jpg')">
@@ -8,18 +8,20 @@
                  width="614"
                  height="409"
                  class="hidden">
+            <time datetime="" class="intro-competition__date">30 Novembre 2018</time>
         </figure>
+
         <div class="intro-competition__bloc wrap">
             <div class="intro-competition__bloc-text">
-                <h3 class="" aria-level="3" role="heading">Informations générales</h3>
+                <h3 class="title title__blue title__left title-size" aria-level="3" role="heading">Informations générales</h3>
                 <div class="intro-competition__bloc-info">
                     <div class="intro-competition__info">
                         <span>Adresse</span>
-                        <p>{{ $competition->place }}</p>
+                        <p class="intro-competition__info-content intro-competition__info-content--place">{{ $competition->place }}</p>
                     </div>
                     <div class="intro-competition__info">
                         <span>Type</span>
-                        <p>Francophone Outdoor</p>
+                        <p class="intro-competition__info-content intro-competition__info-content--type">Francophone Outdoor</p>
                     </div>
 
                 </div>
