b56aab71f425acb1249e8703ef3a31cd75229978
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-04-29 12:50:12 +0200

    Added css for partial infos competition

--- resources/views/partials/single/competition/infos_competition.blade.php
+++ resources/views/partials/single/competition/infos_competition.blade.php
@@ -1,7 +1,7 @@
 <div class="infos-competition wrap">
     <div class="infos-competition__bloc">
         <h3 aria-level="3" role="heading" class="title title__blue title__left title-size">Droits d'inscriptions</h3>
-        <p>
+        <p class="infos-competition__text">
             Pré-inscription : 1 euro par prévente
             Inscription : 3 euros par épreuve
         </p>
@@ -9,27 +9,33 @@
     <div class="infos-competition__bloc">
         <h3 aria-level="3" role="heading" class="title title__blue title__left title-size">Sponsors</h3>
         <div class="infos-competition__bloc-img">
-            <figure class="infos-competition__figure">
-                <img src="../img/logo-redbull.png"
-                     srcset="../img/logo-redbull.png 2x"
-                     alt="Image du sponsor"
-                     width="100"
-                     height="100">
-            </figure>
-            <figure class="infos-competition__figure">
-                <img src="../img/logo-brabant.png"
-                     srcset="../img/logo-brabant.png 2x"
-                     alt="Image du sponsor"
-                     width="142"
-                     height="100">
-            </figure>
-            <figure class="infos-competition__figure">
-                <img src="../img/logo-drink.png"
-                     srcset="../img/logo-drink.png 2x"
-                     alt="Image du sponsor"
-                     width="131"
-                     height="100">
-            </figure>
+            <a href="#1" class="infos-competition__figure" target="_blank">
+                <figure>
+                    <img src="../img/logo-redbull.png"
+                         srcset="../img/logo-redbull.png 2x"
+                         alt="Image du sponsor"
+                         width="100"
+                         height="100">
+                </figure>
+            </a>
+            <a href="#2" target="_blank" class="infos-competition__figure">
+                <figure>
+                    <img src="../img/logo-brabant.png"
+                         srcset="../img/logo-brabant.png 2x"
+                         alt="Image du sponsor"
+                         width="142"
+                         height="100">
+                </figure>
+            </a>
+            <a href="#3" target="_blank" class="infos-competition__figure">
+                <figure>
+                    <img src="../img/logo-drink.png"
+                         srcset="../img/logo-drink.png 2x"
+                         alt="Image du sponsor"
+                         width="131"
+                         height="100">
+                </figure>
+            </a>
         </div>
     </div>
 </div>
