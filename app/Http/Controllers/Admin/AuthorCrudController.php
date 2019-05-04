3d10f7da504da8626776eace9bf140641912927d
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-05-04 12:14:18 +0200

    Added field to add author news

--- app/Http/Controllers/Admin/AuthorCrudController.php
+++ app/Http/Controllers/Admin/AuthorCrudController.php
@@ -25,7 +25,7 @@ class AuthorCrudController extends CrudController
         */
         $this->crud->setModel('App\Models\Author');
         $this->crud->setRoute(config('backpack.base.route_prefix') . '/author');
-        $this->crud->setEntityNameStrings('author', 'authors');
+        $this->crud->setEntityNameStrings('un auteur', 'auteurs');
 
         /*
         |--------------------------------------------------------------------------
@@ -33,8 +33,66 @@ class AuthorCrudController extends CrudController
         |--------------------------------------------------------------------------
         */
 
-        // TODO: remove setFromDb() and manually define Fields and Columns
-        $this->crud->setFromDb();
+        // Columns
+
+        // Firstname Column
+        $this->crud->addColumn(
+            [
+                'name' => 'firstname',
+                'type' => 'text',
+                'label' => 'Prénom de \'auteur'
+            ]
+        );
+
+        // Lastname Column
+        $this->crud->addColumn(
+            [
+                'name' => 'lastname',
+                'type' => 'text',
+                'label' => 'Nom de l\'auteur',
+            ]
+        );
+
+        // Fields
+
+        // Firstname Field
+        $this->crud->addField(
+            [
+                'name' => 'firstname',
+                'type' => 'text',
+                'label' => 'Prénom de l\'auteur'
+            ]
+        );
+
+        // Lastname Field
+        $this->crud->addField(
+            [
+                'name' => 'lastname',
+                'type' => 'text',
+                'label' => 'Nom de l\'auteur'
+            ]
+        );
+
+        //Image Field
+        $this->crud->addField([
+            'name' => "picture",
+            'type' => 'image',
+            'label' => "Photo de présentation",
+            'upload' => true,
+            'crop' => true, // set to true to allow cropping, false to disable
+            'aspect_ratio' => 0, // ommit or set to 0 to allow any aspect ratio
+            // 'disk' => 's3_bucket', // in case you need to show images from a different disk
+            // 'prefix' => 'uploads/images/profile_pictures/' // in case your db value is only the file name (no path), you can use this to prepend your path to the image src (in HTML), before it's shown to the user;
+        ]);
+
+        // Email Field
+        $this->crud->addField(
+            [
+                'name' => 'email',
+                'type' => 'email',
+                'label' => 'Email de l\'auteur'
+            ]
+        );
 
         // add asterisk for fields that are required in AuthorRequest
         $this->crud->setRequiredFields(StoreRequest::class, 'create');
