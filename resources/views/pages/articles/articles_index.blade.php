e02cd8b4983f72d516e1611812c28f2b9f7d438f
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-05-04 14:31:10 +0200

    Added partial intro news

--- resources/views/pages/articles/articles_index.blade.php
+++ resources/views/pages/articles/articles_index.blade.php
@@ -7,6 +7,7 @@
 @section('class', 'index_competitions')
 
 @section('content')
+    @include('partials.intros.intro-article')
     <div class="wrap">
         @include('partials.switchers.switcher-articles')
     </div>
