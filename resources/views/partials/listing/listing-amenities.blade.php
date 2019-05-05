5f87022a0381f58438b24a75c89de9c9c4372c1e
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-05-05 14:42:53 +0200

    Added true content equipments

--- resources/views/partials/listing/listing-amenities.blade.php
+++ resources/views/partials/listing/listing-amenities.blade.php
@@ -1,52 +1,22 @@
 <div class="equipement__container">
-    <div class="equipement__bloc">
-        <figure class="equipement__figure">
-            <span class="equipement__price">33€</span>
-            <h2 aria-level="2" role="heading" class="equipement__title">Sweet shirt blanc</h2>
-            <div class="equipement__size">
-                <span>S - M - L</span>
-            </div>
-            <img src="../img/equipment-one.jpg"
-                 srcset="../img/equipment-one.jpg 2x"
-                 alt="Photo de l'équipement : "
-                 width="335"
-                 height="457">
-        </figure>
-    </div>
-    <div class="equipement__bloc">
-        <figure class="equipement__figure">
-            <div class="equipement__size">
-                <span>S - M - L</span>
-            </div>
-            <img src="../img/equipment-two.jpg"
-                 srcset="../img/equipment-two.jpg 2x"
-                 alt="Photo de l'équipement : "
-                 width="335"
-                 height="457">
-        </figure>
-    </div>
-    <div class="equipement__bloc">
-        <figure class="equipement__figure">
-            <div class="equipement__size">
-                <span>S - M - L</span>
-            </div>
-            <img src="../img/equipment-three.jpg"
-                 srcset="../img/equipment-three.jpg 2x"
-                 alt="Photo de l'équipement : "
-                 width="335"
-                 height="457">
-        </figure>
-    </div>
-    <div class="equipement__bloc">
-        <figure class="equipement__figure">
-            <div class="equipement__size">
-                <span>S - M - L</span>
-            </div>
-            <img src="../img/equipment-four.jpg"
-                 srcset="../img/equipment-four.jpg 2x"
-                 alt="Photo de l'équipement : "
-                 width="335"
-                 height="457">
-        </figure>
-    </div>
+    @foreach($amenities as $amenity)
+        <div class="equipement__bloc">
+            <figure class="equipement__figure">
+                <span class="equipement__price">{{ $amenity->price }}</span>
+                <h2 aria-level="2" role="heading" class="equipement__title">{{ $amenity->title }}</h2>
+                <div class="equipement__size">
+                    @foreach($amenity->sizes as $size)
+                        <span>
+                            {{$size->name}}
+                        </span>
+                    @endforeach
+                </div>
+                <img src="{{ $amenity->getImageEquipment('_profile.jpg') }}"
+                     srcset="{{ $amenity->getImageEquipment('_profile.jpg') }}"
+                     alt="Photo de l'équipement : {{ $amenity->title }}"
+                     width="335"
+                     height="457">
+            </figure>
+        </div>
+    @endforeach
 </div>
