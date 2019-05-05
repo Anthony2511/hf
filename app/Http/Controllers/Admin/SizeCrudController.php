1eb79f92dbfb40a11016ddc9b51f7952c0f141bf
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-05-05 11:37:03 +0200

    Added field to add size

--- app/Http/Controllers/Admin/SizeCrudController.php
+++ app/Http/Controllers/Admin/SizeCrudController.php
@@ -33,8 +33,27 @@ class SizeCrudController extends CrudController
         |--------------------------------------------------------------------------
         */
 
-        // TODO: remove setFromDb() and manually define Fields and Columns
-        $this->crud->setFromDb();
+        // Columns
+
+        // Name Column
+        $this->crud->addColumn(
+            [
+                'name' => 'name',
+                'type' => 'text',
+                'label' => 'Nom de la taille'
+            ]
+        );
+
+        // Fields
+
+        // Name Field
+        $this->crud->addField(
+            [
+                'name' => 'name',
+                'type' => 'text',
+                'label' => 'Nom de la taille'
+            ]
+        );
 
         // add asterisk for fields that are required in SizeRequest
         $this->crud->setRequiredFields(StoreRequest::class, 'create');
