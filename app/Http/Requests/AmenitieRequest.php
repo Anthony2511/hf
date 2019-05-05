4c2aeb38de677f807be479aab2a515b6d53b5b0c
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-05-05 11:53:08 +0200

    Succes add equipment

--- app/Http/Requests/AmenitieRequest.php
+++ app/Http/Requests/AmenitieRequest.php
@@ -26,7 +26,10 @@ class AmenitieRequest extends FormRequest
     public function rules()
     {
         return [
-            // 'name' => 'required|min:5|max:255'
+            'price' => 'required|min:2|max:255',
+            'title' => 'required|min:2|max:255',
+            'sexe' => 'required|min:2|max:255',
+            'image' => 'required',
         ];
     }
 
