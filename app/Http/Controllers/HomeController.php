a22497048c74d8a75c9d0d4249c761b76d3ea177
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-05-09 13:20:22 +0200

    Added 2 first news order by date home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 use App\Models\Athlete;
 use Backpack\PageManager\app\Models\Page;
@@ -19,12 +20,14 @@ class HomeController extends Controller
 
         $page = Page::where('template', 'home_index')->firstOrFail();
         $this->data['athletes'] = Athlete::inRandomOrder()->orderBy('lastname', 'ASC')->limit(3)->get();
+        $this->data['articles'] = Article::orderBy('date', 'ASC')->limit(2)->get();
 
         $this->data['title'] = $page->title;
         $this->data['page'] = $page->withFakes();
 
         return view('pages.' . $page->template, $this->data, [
-            'athletes' => $this->data['athletes']
+            'athletes' => $this->data['athletes'],
+            'articles' => $this->data['articles']
         ]);
 
     }
--- resources/views/partials/home/news-home.blade.php
+++ resources/views/partials/home/news-home.blade.php
@@ -3,72 +3,41 @@
         <h2 class="title title__blue title__center" aria-level="2" role="heading">Dernières actualités</h2>
         <div class="news-home__margin">
             <div class="news-home__container">
-                <div class="news-home__bloc">
-                    <figure class="news-home__figure">
-                        <div class="news-home__bloc-figure">
-                            <time class="news-home__date">16/11/2018</time>
-                            <img src="../img/photo-news.jpg"
-                                 srcset="../img/photo-news.jpg 2x"
-                                 alt="Record du club Martin Reip"
-                                 width="541"
-                                 height="250">
-                        </div>
-                    </figure>
-                    <section class="news-home__sub-bloc">
-                        <div class="news-home__author">
-                            <figure class="news-home__author-figure">
-                                <img src="../img/author.jpg"
-                                     srcset="../img/author.jpg 2x"
-                                     alt="Photo de l'auteur : Anthony Beaumecker"
-                                     width="30"
-                                     height="30">
-                            </figure>
-                            <span class="news-home__author-name">par <span>Anthony Beaumecker</span></span>
-                        </div>
-                        <h3 aria-level="3" role="heading" class="news-home__title">Record du club Martin Reip</h3>
-                        <p class="news-home__text">
-                            Bravo à Martin Reip qui termine son octathlon, à
-                            Schaerbeek le we dernier, avec un total de 4622 points.
-                            Record personnel et surtout nouveau record du club! Bravo
-                            à lui :-)
-                        </p>
-                        <span class="news-home__comments"> 3 commentaires</span>
-                    </section>
-                    <a href="#" class="news-home__link"></a>
-                </div>
-                <div class="news-home__bloc">
-                    <figure class="news-home__figure">
-                        <div class="news-home__bloc-figure">
-                            <time class="news-home__date">16/11/2018</time>
-                            <img src="../img/photo-news.jpg"
-                                 srcset="../img/photo-news.jpg 2x"
-                                 alt="Record du club Martin Reip"
-                                 width="541"
-                                 height="250">
-                        </div>
-                    </figure>
-                    <section class="news-home__sub-bloc">
-                        <div class="news-home__author">
-                            <figure class="news-home__author-figure">
-                                <img src="../img/author.jpg"
-                                     srcset="../img/author.jpg 2x"
-                                     alt="Photo de l'auteur : Anthony Beaumecker"
-                                     width="30"
-                                     height="30">
-                            </figure>
-                            <span class="news-home__author-name">par <span>Anthony Beaumecker</span></span>
-                        </div>
-                        <h3 aria-level="3" role="heading" class="news-home__title">Record du club Martin Reip</h3>
-                        <p class="news-home__text">
-                            Bravo à Martin Reip qui termine son octathlon, à
-                            Schaerbeek le we dernier, avec un total de 4622 points.
-                            Record personnel et surtout nouveau record du club! Bravo
-                            à lui :-)
-                        </p>
-                        <span class="news-home__comments">3 commentaires</span>
-                    </section>
-                    <a href="#" class="news-home__link"></a>
-                </div>
+                @foreach($articles as $article)
+                    <div class="news-home__bloc">
+                        <figure class="news-home__figure">
+                            <div class="news-home__bloc-figure">
+                                <time class="news-home__date">16/11/2018</time>
+                                <img src="../img/photo-news.jpg"
+                                     srcset="../img/photo-news.jpg 2x"
+                                     alt="Record du club Martin Reip"
+                                     width="541"
+                                     height="250">
+                            </div>
+                        </figure>
+                        <section class="news-home__sub-bloc">
+                            <div class="news-home__author">
+                                <figure class="news-home__author-figure">
+                                    <img src="../img/author.jpg"
+                                         srcset="../img/author.jpg 2x"
+                                         alt="Photo de l'auteur : Anthony Beaumecker"
+                                         width="30"
+                                         height="30">
+                                </figure>
+                                <span class="news-home__author-name">par <span>Anthony Beaumecker</span></span>
+                            </div>
+                            <h3 aria-level="3" role="heading" class="news-home__title">{{ $article->title }}</h3>
+                            <p class="news-home__text">
+                                Bravo à Martin Reip qui termine son octathlon, à
+                                Schaerbeek le we dernier, avec un total de 4622 points.
+                                Record personnel et surtout nouveau record du club! Bravo
+                                à lui :-)
+                            </p>
+                            <span class="news-home__comments"> 3 commentaires</span>
+                        </section>
+                        <a href="#" class="news-home__link"></a>
+                    </div>
+                @endforeach
             </div>
             <a href="{{route('articles')}}" class="button">
                 <span class="button-blue__left">Voir toutes les actualités</span>
