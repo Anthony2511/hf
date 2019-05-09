f2893346b20458804361618054236a3924bf16f1
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-05-09 10:00:23 +0200

    Added condition if content training is null

--- resources/views/partials/listing/listing-trainings.blade.php
+++ resources/views/partials/listing/listing-trainings.blade.php
@@ -7,18 +7,25 @@
                     <div class="trainings__intro">
                         <span class="trainings__day">{{ $training->day }}</span>
                         <span class="trainings__hour">
-                            {{ date("H", strtotime($training->startHour)) }}h{{ date("i", strtotime($training->startHour)) }}
+                            {{ date("H", strtotime($training->startHour)) }}
+                            h{{ date("i", strtotime($training->startHour)) }}
                             -
-                            {{ date("H", strtotime($training->endHour)) }}h{{ date("i", strtotime($training->endHour)) }}</span>
+                            {{ date("H", strtotime($training->endHour)) }}
+                            h{{ date("i", strtotime($training->endHour)) }}</span>
                     </div>
                     <div class="trainings__infos">
                         <span class="trainings__place">{{$training->place->name}}</span>
                         <span class="trainings__type trainings__type--sprint">{{$training->type->name}}</span>
                     </div>
-                    <p class="trainings__text">
-                        {{$training->content}}
-                    </p>
-
+                    @if(!empty ($training->content))
+                        <p class="trainings__text">
+                            {{$training->content}}
+                        </p>
+                    @else
+                        <p class="trainings__text">
+                            Pas de contenu pour l'instant.
+                        </p>
+                    @endif
                     @if(!empty ($training->trainer))
                         <a href="{{url('entraineurs/' . $training->trainer['slug'] )}}" class="trainings__trainers">
                             <figure class="trainings__figure">
