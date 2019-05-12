a70f3380d133d906a0a97b1713da5c9858021b2f
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-05-12 11:25:04 +0200

    Added css partial comments

--- resources/views/partials/single/article/comment_article.blade.php
+++ resources/views/partials/single/article/comment_article.blade.php
@@ -1,3 +1,67 @@
-<section class="article-comment">
+<section class="article-comment wrap">
     <h2 class="title title__blue title__center" aria-level="2" role="heading">Les commentaires</h2>
+    <div class="article-comment__container">
+        <div class="article-comment__author">
+            <div class="article-comment__author-bloc">
+                <figure class="article-comment__author-figure">
+                    <img src="../img/author.jpg"
+                         srcset="../img/author.jpg 2x"
+                         alt="Photo de l'auteur"
+                         width="30"
+                         height="30">
+                </figure>
+                <div class="article-comment__author-infos">
+                    <span class="article-comment__author-name">Anthony Beaumecker</span>
+                    <time class="article-comment__date-author">Le 14 janvier 2019 à 14h59</time>
+                </div>
+            </div>
+            <p class="article-comment__text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta felis euismod, tristique est sit amet, luctus arcu.
+                Vestibulum risus tortor, volutpat non ex ac, cursus efficitur lacus. Nunc dui augue, maximus tincidunt cursus eu, fringilla ac neque.
+                Donec varius vehicula eros. Aenean semper, dui sed ullamcorper commodo, quam eros laoreet lorem, ut bibendum arcu purus sit amet lacus.
+            </p>
+        </div>
+        <div class="article-comment__bloc-container">
+            <div class="article-comment__author-line">
+                <div class="article-comment__author-bloc">
+                    <figure class="article-comment__author-figure">
+                        <img src="../img/author.jpg"
+                             srcset="../img/author.jpg 2x"
+                             alt="Photo de l'auteur"
+                             width="30"
+                             height="30">
+                    </figure>
+                    <div class="article-comment__author-infos">
+                        <span class="article-comment__author-name">Anthony Beaumecker</span>
+                        <time class="article-comment__date-author">Le 14 janvier 2019 à 14h59</time>
+                    </div>
+                </div>
+                <p class="article-comment__text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta felis euismod, tristique est sit amet, luctus arcu.
+                    Vestibulum risus tortor, volutpat non ex ac, cursus efficitur lacus. Nunc dui augue, maximus tincidunt cursus eu, fringilla ac neque.
+                    Donec varius vehicula eros. Aenean semper, dui sed ullamcorper commodo, quam eros laoreet lorem, ut bibendum arcu purus sit amet lacus.
+                </p>
+            </div>
+            <div class="article-comment__author-line">
+                <div class="article-comment__author-bloc">
+                    <figure class="article-comment__author-figure">
+                        <img src="../img/author.jpg"
+                             srcset="../img/author.jpg 2x"
+                             alt="Photo de l'auteur"
+                             width="30"
+                             height="30">
+                    </figure>
+                    <div class="article-comment__author-infos">
+                        <span class="article-comment__author-name">Anthony Beaumecker</span>
+                        <time class="article-comment__date-author">Le 14 janvier 2019 à 14h59</time>
+                    </div>
+                </div>
+                <p class="article-comment__text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta felis euismod, tristique est sit amet, luctus arcu.
+                    Vestibulum risus tortor, volutpat non ex ac, cursus efficitur lacus. Nunc dui augue, maximus tincidunt cursus eu, fringilla ac neque.
+                    Donec varius vehicula eros. Aenean semper, dui sed ullamcorper commodo, quam eros laoreet lorem, ut bibendum arcu purus sit amet lacus.
+                </p>
+            </div>
+        </div>
+    </div>
 </section>
