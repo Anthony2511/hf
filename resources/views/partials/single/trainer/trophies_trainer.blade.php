089812f28af990511d5bd5048cf15d9695da1e55
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-05-12 15:44:47 +0200

    Added foreach listing trophies trainers

--- resources/views/partials/single/trainer/trophies_trainer.blade.php
+++ resources/views/partials/single/trainer/trophies_trainer.blade.php
@@ -1,14 +1,15 @@
 <section class="athlete-trophies wrap">
     <h4 class="title title__blue title__left title-size" aria-level="4" role="heading">Ses palmarès</h4>
     <div class="athlete-trophies__container">
-        <div class="athlete-trophies__bloc">
-            <datetime class="athlete-trophies__date">2014</datetime> - <span>Entraîneur LBFA de l'année</span>
-        </div>
-        <div class="athlete-trophies__bloc">
-            <datetime class="athlete-trophies__date">2017</datetime> - <span>Entraîneur du mois</span>
-        </div>
-        <div class="athlete-trophies__bloc">
-            <datetime class="athlete-trophies__date">2018</datetime> - <span>3ième meileur entraîneur provincial</span>
-        </div>
+        @if(!empty (count($trainer->trophies)))
+            @foreach($trainer->trophies as $trophie)
+                <div class="athlete-trophies__bloc">
+                    <time class="athlete-trophies__date" datetime="{{ $trophie->year }}">{{ $trophie->year }}</time>
+                    - <span>{{ $trophie->name }}</span>
+                </div>
+            @endforeach
+        @else
+            <p class="empty">L'entraîneur n'a actuellement aucun trophée à son actif.</p>
+        @endif
     </div>
 </section>
