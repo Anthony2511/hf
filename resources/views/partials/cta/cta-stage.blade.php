095d16c319eb9142139011f5886eca20750b2431
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-05-16 12:36:21 +0200

    Added true content partial single stage

--- resources/views/partials/cta/cta-stage.blade.php
+++ resources/views/partials/cta/cta-stage.blade.php
@@ -1,20 +1,13 @@
 <div class="cta-news cta-news__background">
     <div class="wrap">
-        <h2 aria-level="2" role="heading" class="title title__blue title__center">Envie de commencer la course à pied ?</h2>
+        <h2 aria-level="2" role="heading" class="title title__blue title__center">Pas encore d'équipement?</h2>
         <p class="cta-page__text">
-            Le club des Hautes Fagnes organise depuis maintenant plus de 30 ans le jogging de la St Sylvestre
-            (8km légèrement vallonné et varié - route, chemin, sentiers). Chaque année, c'est un moment
-            incontournable de la saison de route qui se déroule toujours le dernier dimanche de l'année au stade de Bielmont.
+            Le club des Hautes Fagnes offre une grande variété en terme d'équipements,n'hésitez pas à aller jeter un oeil
+            sur notre page d'équipements, et vous aussi venez porter fièrement les couleurs du club
         </p>
-        <a href="{{ route('stages') }}" class="button" title="Vers les stages">
-            <span class="button-blue__left">Voir les stages</span>
+        <a href="{{ route('equipements') }}" class="button" title="Vers les stages">
+            <span class="button-blue__left">Voir nos équipements</span>
             <i class="button-blue__right"></i>
         </a>
     </div>
-</div><?php
-/**
- * Created by PhpStorm.
- * User: Anthony
- * Date: 16/05/2019
- * Time: 12:31
- */
+</div>
