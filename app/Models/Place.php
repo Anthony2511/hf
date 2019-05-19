ccdbd17030001c53be55f2f7306f382ce8445e6c
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-05-19 16:23:09 +0200

    Added slug for types & places trainings

--- app/Models/Place.php
+++ app/Models/Place.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Backpack\CRUD\CrudTrait;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Cviebrock\EloquentSluggable\SluggableScopeHelpers;
 
 class Place extends Model
 {
     use CrudTrait;
+    use Sluggable, SluggableScopeHelpers;
 
     /*
     |--------------------------------------------------------------------------
@@ -21,6 +24,7 @@ class Place extends Model
     // protected $guarded = ['id'];
     protected $fillable = array(
         'name',
+        'slug'
     );
     // protected $hidden = [];
     // protected $dates = [];
@@ -30,6 +34,19 @@ class Place extends Model
      *
      * @return array
      */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'slug_or_title',
+            ],
+        ];
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 
     /*
     |--------------------------------------------------------------------------
@@ -58,6 +75,17 @@ class Place extends Model
     | ACCESORS
     |--------------------------------------------------------------------------
     */
+    // The slug is created automatically from the "title" field if no slug exists.
+    public function getSlugOrTitleAttribute()
+    {
+        if ($this->slug != '') {
+            return $this->slug;
+        }
+
+        $name = $this->name;
+
+        return $name;
+    }
 
     /*
     |--------------------------------------------------------------------------
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Backpack\CRUD\CrudTrait;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Cviebrock\EloquentSluggable\SluggableScopeHelpers;
 
 class Type extends Model
 {
     use CrudTrait;
+    use Sluggable, SluggableScopeHelpers;
 
     /*
     |--------------------------------------------------------------------------
@@ -20,10 +23,30 @@ class Type extends Model
     public $timestamps = true;
     // protected $guarded = ['id'];
     protected $fillable = array(
-        'name'
+        'name',
+        'slug'
     );
     // protected $hidden = [];
     // protected $dates = [];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'slug_or_title',
+            ],
+        ];
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 
     /*
     |--------------------------------------------------------------------------
@@ -52,6 +75,17 @@ class Type extends Model
     | ACCESORS
     |--------------------------------------------------------------------------
     */
+    // The slug is created automatically from the "title" field if no slug exists.
+    public function getSlugOrTitleAttribute()
+    {
+        if ($this->slug != '') {
+            return $this->slug;
+        }
+
+        $name = $this->name;
+
+        return $name;
+    }
 
     /*
     |--------------------------------------------------------------------------
