b6abc4159a308a5681773d11f0a884533d043628
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-05-21 14:03:58 +0200

    Added partial join-form to view join_index

--- resources/views/pages/join_index.blade.php
+++ resources/views/pages/join_index.blade.php
@@ -4,10 +4,11 @@
     @include('partials.header')
 @endsection
 
-@section('class', 'index_athletes')
+@section('class', 'index_about')
 
 @section('content')
     <div class="about__background">
         @include('partials.join.join_intro')
     </div>
+    @include('partials.join.join_contact')
 @endsection
---
+++ resources/views/partials/join/join_contact.blade.php
@@ -0,0 +1,73 @@
+<section class="single-stage__bloc-form wrap">
+    <h2 aria-level="2" role="heading" class="title title__blue title__center">Inscrivez-vous&hellip;</h2>
+    {{ Form::open([ 'method' => 'POST', 'class' => 'single-stage__form', 'route' =>['mail-contact-form']]) }}
+    <div class="single-stage__form-container">
+        <legend class="single-stage__form-legend">Tous les champs suivis d'un (*) sont obligatoires</legend>
+        <div class="single-stage__form-bloc">
+            <div class="single-stage__form-width">
+                <div class="floating-label floating-label__width">
+                    <input class="single-stage__form-input" placeholder="Votre prénom *"
+                           type="text" name="firstname" id="firstname" autocomplete="off" required>
+                    <label for="firstname">Votre prénom *</label>
+                    @if($errors->has('firstname'))
+                        <span class="form-error">{{$errors->first('firstname')}}</span>
+                    @endif
+                </div>
+                <div class="floating-label floating-label__width">
+                    <input class="single-stage__form-input" placeholder="Votre nom *"
+                           type="text" name="lastname" id="lastname" autocomplete="off" required>
+                    <label for="lastname">Votre nom *</label>
+                    @if($errors->has('lastname'))
+                        <span class="form-error"> {{$errors->first('lastname')}}</span>
+                    @endif
+                </div>
+            </div>
+            <div class="floating-label">
+                <input class="single-stage__form-input" placeholder="Votre email *"
+                       type="text" name="email" id="email" autocomplete="off" required>
+                <label for="email">Votre email *</label>
+                @if($errors->has('email'))
+                    <span class="form-error">{{$errors->first('email')}}</span>
+                @endif
+            </div>
+            <div class="single-stage__form-width">
+                <div class="floating-label floating-label__width">
+                    <input class="single-stage__form-input" placeholder="Votre téléphone"
+                           type="text" name="phone" id="phone" autocomplete="off" required>
+                    <label for="phone">Votre téléphone</label>
+                    @if($errors->has('phone'))
+                        <span class="form-error">{{$errors->first('phone')}}</span>
+                    @endif
+                </div>
+                <div class="floating-label floating-label__width">
+                    <input class="single-stage__form-input" placeholder="Votre date de naissance*"
+                           type="date" name="date" id="date" autocomplete="off"
+                           value="1919-01-01"
+                           min="1919-01-01" max="2015-01-01">
+                    <label for="date">Votre date de naissance*</label>
+                    @if($errors->has('date'))
+                        <span class="form-error">{{$errors->first('date')}}</span>
+                    @endif
+                </div>
+            </div>
+            <div class="floating-label">
+                <textarea class="single-stage__form-textarea" name="bodyMessage" id="bodyMessage" cols="30" rows="10"
+                          required placeholder="Vos éventuelles questions ?"></textarea>
+                <label for="bodyMessage" class="floating-label__label">Vos éventuelles questions ? </label>
+                @if($errors->has('bodyMessage'))
+                    <span class="form-error">{{$errors->first('bodyMessage')}}</span>
+                @endif
+            </div>
+            <button type="submit" class="button">
+                <span class="button-orange__left">Envoyez votre inscription</span>
+                <i class="button-orange__right-send"></i>
+            </button>
+            @if((session('success')))
+                <p class="form-success">{!! session('success') !!}</p>
+            @else
+            @endif
+        </div>
+
+    </div>
+    {{ Form::close() }}
+</section>
