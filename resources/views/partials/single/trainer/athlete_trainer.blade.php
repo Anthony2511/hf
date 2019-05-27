7d378784c5fddddf47cdf3d484705c249f149184
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-05-27 12:34:10 +0200

    Hover animation athlete trainer

--- resources/views/partials/single/trainer/athlete_trainer.blade.php
+++ resources/views/partials/single/trainer/athlete_trainer.blade.php
@@ -2,7 +2,7 @@
     <h4 class="title title__blue title__center title-size" aria-level="4" role="heading">Ses athlètes</h4>
     <div class="athlete-trainer__bloc">
         @if(!empty (count($trainer->athletes)))
-            @foreach($trainer->athletes as $athlete)
+            @foreach($trainer->athletes->take(5) as $athlete)
                 <a href="{{url('athletes/' . $athlete['slug'])}}"
                    class="athlete-trainer__athlete-link">
                     <div class="athlete-trainer__athlete">
