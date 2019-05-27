f04bc285ea116a925699f635a1b3bf6fa2a2c50e
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-05-27 14:51:38 +0200

    Foreach listing athlete same division

--- resources/views/partials/single/athlete/division_athlete.blade.php
+++ resources/views/partials/single/athlete/division_athlete.blade.php
@@ -1,8 +1,23 @@
-<section class="athlete-division wrap">
+<section class="athlete-trainer__container wrap">
     <h4 class="title title__blue title__center title-size" aria-level="4" role="heading">Athlètes de la même division</h4>
-    @foreach($athleteDivision as $athlete)
-        <p>
-            {{$athlete->firstname}}
-        </p>
-    @endforeach
+    <div class="athlete-trainer__bloc">
+        @if(!empty (count($athleteDivision)))
+            @foreach($athleteDivision->take(5) as $athlete)
+                <a href="{{url('athletes/' . $athlete['slug'])}}"
+                   class="athlete-trainer__athlete-link">
+                    <div class="athlete-trainer__athlete">
+                        <figure class="athlete-trainer__athlete-figure">
+                            <img src="{{ $athlete->getImageProfile('_profile.jpg') }}"
+                                 alt="Photo de l'athlète : {{$athlete->fullname}}"
+                                 width="335"
+                                 height="457">
+                        </figure>
+                        <span class="athlete-trainer__athlete-name">{{$athlete->fullname}}</span>
+                    </div>
+                </a>
+            @endforeach
+        @else
+            <p class="empty">Aucun athlète</p>
+        @endif
+    </div>
 </section>
