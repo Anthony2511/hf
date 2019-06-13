01345585ea324fccf83398c3fcb51ffc89c4f262
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-06-13 12:57:12 +0200

    Background dashboard

--- resources/views/vendor/backpack/base/inc/main_header.blade.php
+++ resources/views/vendor/backpack/base/inc/main_header.blade.php
@@ -1,13 +1,13 @@
 <header class="main-header">
   <!-- Logo -->
-  <a href="{{ url('') }}" class="logo">
+  <a href="{{ url('') }}" class="logo" style="background-color:#2C3E50">
     <!-- mini logo for sidebar mini 50x50 pixels -->
     <span class="logo-mini">{!! config('backpack.base.logo_mini') !!}</span>
     <!-- logo for regular state and mobile devices -->
     <span class="logo-lg">{!! config('backpack.base.logo_lg') !!}</span>
   </a>
   <!-- Header Navbar: style can be found in header.less -->
-  <nav class="navbar navbar-static-top" role="navigation">
+  <nav class="navbar navbar-static-top" role="navigation" style="background-color:#34495E">
     <!-- Sidebar toggle button-->
     <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
       <span class="sr-only">{{ trans('backpack::base.toggle_navigation') }}</span>
